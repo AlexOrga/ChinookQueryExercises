b205c5ed7b42f115ceb5da937d8b3dec156e7954
Alex Orga <alexorga.dev@gmail.com>
2018-10-16 19:30:42 -0500

    third query

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -2,14 +2,27 @@
 -- who are not in the US.
 
 SELECT 
-	FullName = FirstName + '' + LastName,
-	CustomerId, 
-	Country 
-FROM Customer 
-WHERE Country != 'USA'
+	FullName = c.FirstName + '' + c.LastName,
+	c.CustomerId, 
+	c.Country 
+FROM Customer c
+WHERE c.Country != 'USA'
 
 -- brazil_customers.sql: Provide a query only showing the Customers from Brazil.
 
 SELECT *
-FROM Customer
+FROM Customer c
+WHERE c.Country = 'Brazil'
+
+--brazil_customers_invoices.sql: Provide a query showing the Invoices of customers who are from Brazil. 
+--The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
+
+SELECT 
+	FullName = c.FirstName + '' + c.LastName,
+	i.InvoiceId,
+	i.InvoiceDate,
+	i.BillingCountry
+FROM Customer c 
+INNER JOIN Invoice i
+ON c.CustomerId = i.CustomerId
 WHERE Country = 'Brazil'
