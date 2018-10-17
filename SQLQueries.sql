134d4e5ba0642e07be5a7bf42c00ed7eceac393f
Alex Orga <alexorga.dev@gmail.com>
2018-10-16 19:25:02 -0500

    fifth Query

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -32,3 +32,9 @@ WHERE Country = 'Brazil'
 SELECT *
 FROM Employee e
 WHERE e.Title = 'Sales Support Agent'
+
+--unique_invoice_countries.sql: Provide a query showing a unique/distinct list of billing countries from the Invoice table.
+
+SELECT DISTINCT 
+	i.BillingCountry
+FROM Invoice i
